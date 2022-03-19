5ec010d10dbbaafd656fd324a6cd6d8211b64443
Daniyal Tariq <daniyal.tariq@students.iaac.net>
2022-03-19 17:44:57 +0500

    update .7

--- src/examples/perlin-noise/script.js
+++ src/examples/perlin-noise/script.js
@@ -4,39 +4,15 @@ import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/exampl
 import { Rhino3dmLoader } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js";
 import rhino3dm from "https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js";
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // set up loader for converting the results to threejs
 const loader = new Rhino3dmLoader();
 loader.setLibraryPath("https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/");
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 const definition = "perlin_maze.gh";
 
-// setup input change events
-const radius_slider = document.getElementById("radius");
-radius_slider.addEventListener("mouseup", onSliderChange, false);
-radius_slider.addEventListener("touchend", onSliderChange, false);
-const ucount_slider = document.getElementById("ucount");
-ucount_slider.addEventListener("mouseup", onSliderChange, false);
-ucount_slider.addEventListener("touchend", onSliderChange, false);
-const vcount_slider = document.getElementById("vcount");
-vcount_slider.addEventListener("mouseup", onSliderChange, false);
-vcount_slider.addEventListener("touchend", onSliderChange, false);
-const time_slider = document.getElementById("time");
-time_slider.addEventListener("mouseup", onSliderChange, false);
-time_slider.addEventListener("touchend", onSliderChange, false);
-const scale_slider = document.getElementById("scale");
-scale_slider.addEventListener("mouseup", onSliderChange, false);
-scale_slider.addEventListener("touchend", onSliderChange, false);
-const seed_slider = document.getElementById("seed");
-seed_slider.addEventListener("mouseup", onSliderChange, false);
-seed_slider.addEventListener("touchend", onSliderChange, false);
-const start_slider = document.getElementById("start");
-start_slider.addEventListener("mouseup", onSliderChange, false);
-start_slider.addEventListener("touchend", onSliderChange, false);
-const tube_slider = document.getElementById("tube");
-tube_slider.addEventListener("mouseup", onSliderChange, false);
-tube_slider.addEventListener("touchend", onSliderChange, false);
 let rhino, doc;
 rhino3dm().then(async (m) => {
   console.log("Loaded rhino3dm.");
@@ -47,45 +23,117 @@ rhino3dm().then(async (m) => {
 downloadButton
 const downloadButton = document.getElementById("downloadButton")
 downloadButton.onclick = download
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Call appserver
+ * Gets <input> elements from html and sets handlers
+ * (html is generated from the grasshopper definition)
  */
+ function getInputs() {
+  const inputs = {}
+  for (const input of document.getElementsByTagName('input')) {
+    switch (input.type) {
+      case 'number':
+        inputs[input.id] = input.valueAsNumber
+        input.onchange = onSliderChange
+        break
+      case 'range':
+        inputs[input.id] = input.valueAsNumber
+        input.onmouseup = onSliderChange
+        input.ontouchend = onSliderChange
+        break
+      case 'checkbox':
+        inputs[input.id] = input.checked
+        input.onclick = onSliderChange
+        break
+      default:
+        break
+    }
+  }
+  return inputs
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// more globals
+let scene, camera, renderer, controls
+ /**
+  * Sets up the scene, camera, renderer, lights and controls and starts the animation
+  */
+  function init() {
+ 
+    // Rhino models are z-up, so set this as the default
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+
+    // create a scene and a camera
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(1, 1, 1)
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.set(1, -1, 1) // like perspective view
+
+    // very light grey for background, like rhino
+    scene.background = new THREE.Color('whitesmoke')
+
+    // create the renderer and add it to the html
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    // add some controls to orbit the camera
+    controls = new OrbitControls(camera, renderer.domElement)
+
+    // add a directional light
+    const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    directionalLight.intensity = 2
+    scene.add( directionalLight )
+
+    const ambientLight = new THREE.AmbientLight()
+    scene.add( ambientLight )
+
+    // handle changes in the window size
+    window.addEventListener( 'resize', onWindowResize, false )
+
+    animate();
+  }
+  function animate() {
+    scene.traverse(function(child){
+      if (child.isMesh){
+        child.rotation.y +=0.0006
+        child.rotation.z +=0.0006
+        child.rotation.x +=0.0006
+      }
+      //else{(child.ispoint)
+        //child.rotation.y +=0.0008
+        //child.rotation.z +=0.0008
+        //child.rotation.x +=0.0008      }
+      else{(child.isLine)
+        child.rotation.y +=0.0006
+        child.rotation.z +=0.0006
+        child.rotation.x +=0.0006
+      }})
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 async function compute() {
-  showSpinner(true);
-
-  // initialise 'data' object that will be used by compute()
-  const data = {
-    definition: definition,
-    inputs: {
-      radius: radius_slider.valueAsNumber,
-      ucount: ucount_slider.valueAsNumber,
-      vcount: vcount_slider.valueAsNumber,
-      time: time_slider.valueAsNumber,
-      scale: scale_slider.valueAsNumber,
-      seed: seed_slider.valueAsNumber,
-      start : start_slider.valueAsNumber,
-      tube: tube_slider.valueAsNumber,
-      
-    },
-  };
-
-  console.log(data.inputs);
-
-  const request = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  };
-
+  // construct url for GET /solve/definition.gh?name=value(&...)
+  const url = new URL('/solve/' + data.definition, window.location.origin)
+  Object.keys(data.inputs).forEach(key => url.searchParams.append(key, data.inputs[key]))
+  console.log(url.toString())
+  
   try {
-    const response = await fetch("/solve", request);
+    const response = await fetch(url)
+  
+    if(!response.ok) {
+      // TODO: check for errors in response json
+      throw new Error(response.statusText)
+    }
 
-    if (!response.ok) throw new Error(response.statusText);
+    const responseJson = await response.json()
 
-    const responseJson = await response.json();
-    collectResults(responseJson);
-  } catch (error) {
-    console.error(error);
+    collectResults(responseJson)
+
+  } catch(error) {
+    console.error(error)
   }
 }
 
@@ -188,84 +236,40 @@ return null
 * Called when a slider value changes in the UI. Collect all of the
 * slider values and call compute to solve for a new scene
 */
-function onSliderChange() {
-// show spinner
-showSpinner(true);
-compute();
-}
+function onSliderChange () {
+  showSpinner(true)
+  // get slider values
+  let inputs = {}
+  for (const input of document.getElementsByTagName('input')) {
+    switch (input.type) {
+    case 'number':
+      inputs[input.id] = input.valueAsNumber
+      break
+    case 'range':
+      inputs[input.id] = input.valueAsNumber
+      break
+    case 'checkbox':
+      inputs[input.id] = input.checked
+      break
+    }
+  }
+  
+  data.inputs = inputs
 
+  compute()
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * The animation loop!
+ */
+ function animate() {
+  requestAnimationFrame( animate )
+  controls.update()
+  renderer.render(scene, camera)
+}
 /**
 * Shows or hides the loading spinner
 */
-function showSpinner(enable) {
-if (enable) document.getElementById("loader").style.display = "block";
-else document.getElementById("loader").style.display = "none";
-}
- // more globals
- let scene, camera, renderer, controls
-
- /**
-  * Sets up the scene, camera, renderer, lights and controls and starts the animation
-  */
- function init() {
- 
-     // Rhino models are z-up, so set this as the default
-     THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
- 
-     // create a scene and a camera
-     scene = new THREE.Scene()
-     scene.background = new THREE.Color(1, 1, 1)
-     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
-     camera.position.set(1, -1, 1) // like perspective view
- 
-     // very light grey for background, like rhino
-     scene.background = new THREE.Color('whitesmoke')
- 
-     // create the renderer and add it to the html
-     renderer = new THREE.WebGLRenderer({ antialias: true })
-     renderer.setPixelRatio( window.devicePixelRatio )
-     renderer.setSize(window.innerWidth, window.innerHeight)
-     document.body.appendChild(renderer.domElement)
- 
-     // add some controls to orbit the camera
-     controls = new OrbitControls(camera, renderer.domElement)
- 
-     // add a directional light
-     const directionalLight = new THREE.DirectionalLight( 0xffffff )
-     directionalLight.intensity = 2
-     scene.add( directionalLight )
- 
-     const ambientLight = new THREE.AmbientLight()
-     scene.add( ambientLight )
- 
-     // handle changes in the window size
-     window.addEventListener( 'resize', onWindowResize, false )
- 
-     animate();
- 
- 
-   }
- 
-   function animate() {
-     scene.traverse(function(child){
-       if (child.isMesh){
-         child.rotation.y +=0.0006
-         child.rotation.z +=0.0006
-         child.rotation.x +=0.0006
-       }
-       //else{(child.ispoint)
-         //child.rotation.y +=0.0008
-         //child.rotation.z +=0.0008
-         //child.rotation.x +=0.0008      }
-       else{(child.isLine)
-         child.rotation.y +=0.0006
-         child.rotation.z +=0.0006
-         child.rotation.x +=0.0006
-       }})
-     requestAnimationFrame(animate);
-     renderer.render(scene, camera);
-   }
- 
  function onWindowResize() {
  camera.aspect = window.innerWidth / window.innerHeight;
  camera.updateProjectionMatrix();
@@ -323,3 +327,9 @@ else document.getElementById("loader").style.display = "none";
      link.download = filename
      link.click()
  }
+ function showSpinner(enable) {
+  if (enable)
+    document.getElementById('loader').style.display = 'block'
+  else
+    document.getElementById('loader').style.display = 'none'
+}
